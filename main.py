a3b4cde3f0d0a832aa936a643ff9947a8e7e77a2
Michael Wright [sc19mw] <sc19mw@uol-pc-051653.leeds.ac.uk>
2022-11-30 14:08:21 +0000

    print matrix view

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib
+import numpy as np
 
 G = nx.Graph()
 G.add_node("A")
 G.add_node("B")
 G.add_node("C")
-G.add_node("D")
 
 G.add_edge("A","B", weight=3)
 G.add_edge("A","C", weight=5)
 G.add_edge("B","C", weight=1)
 
 lp1 = nx.laplacian_matrix(G)
-print(lp1.get_shape())
 
-lp2 = nx.laplacian_matrix(G, weight="weight")
-print(lp2)
+def matPrintView(matrix):
+    size = matrix.shape
+    for i in range(size[0]):
+        for j in range(size[1]):
+            print(matrix[i,j], " ",end = '')
+        print()
+
+matPrintView(lp1)
+
 
 
 #ax = plt.subplot(121)
