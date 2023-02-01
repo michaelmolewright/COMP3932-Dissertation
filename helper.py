ff279c61fd5b0fa1a47e3f8fa82896c8ceef2920
Michael Wright <sc19mw@leeds.ac.uk>
2023-02-01 17:30:14 +0000

    more stuff

---
+++ helper.py
@@ -0,0 +1,44 @@
+
+import networkx as nx
+from sklearn.cluster import KMeans
+
+import matplotlib.pyplot as plt
+import matplotlib
+
+def makeMoonsGraph(X):
+    
+    G = nx.Graph()
+    for i,node in enumerate(X):
+        G.add_node(i, pos=(node[0],node[1]))
+
+    for i,node in enumerate(X):
+        for j in range(i+1,len(X)):
+            dx = node[0] - X[j][0]
+            dy = node[1] - X[j][1]
+            dist = (dx**2 + dy**2)**0.5
+            w = 2.781 ** ( ((dist ** 2)) / 2)
+            G.add_edge(i,j,weight = dist)
+    return G
+
+def findBestEigen(eigVecs, Correct_labels, X):
+    new_x = []
+    new_y = []
+
+    for r in X:
+        new_x.append(r[0])
+        new_y.append(r[1])
+    kmeans = KMeans(n_clusters=2)
+
+    for count, eigVec in enumerate(eigVecs):
+        new_data = eigVec.reshape(-1,1)
+
+        kmeans.fit(new_data)
+        correct = 0
+        for i, label in enumerate(Correct_labels):
+            if label == kmeans.labels_[i]:
+                correct += 1
+            
+        accuracy = correct/len(Correct_labels)
+        print("EigenVector ", count, " Accuracy = ", accuracy)
+        plt.scatter(new_x, new_y, c=kmeans.labels_)
+        plt.savefig("moons.png")
--- main.py
+++ main.py
@@ -2,41 +2,28 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
+import helper as help
 
 from sklearn.datasets import make_moons
+from sklearn.cluster import KMeans
 
-X, Y = make_moons(n_samples=20, noise=0)
-print(X)
-print(Y)
+X, Y = make_moons(n_samples=100, noise=0)
 
-G = nx.Graph()
-for i,node in enumerate(X):
-    G.add_node(i, pos=(node[0],node[1]))
-
-for i,node in enumerate(X):
-    for j in range(i+1,len(X)):
-        dx = node[0] - X[j][0]
-        dy = node[0] - X[j][1]
-        dist = (dx**2 + dy**2)**0.5
-        G.add_edge(i,j,weight=dist)    
-
-#L = nx.laplacian_matrix(G)
-
-def matPrintView(matrix):
-    size = matrix.shape
-    for i in range(size[0]):
-        for j in range(size[1]):
-            print(matrix[i,j], " ",end = '')
-        print()
-
+moonsGraph = help.makeMoonsGraph(X)
+L = nx.laplacian_matrix(moonsGraph)
+A = nx.adjacency_matrix(moonsGraph)
+Lnorm = nx.normalized_laplacian_matrix(moonsGraph)
 #matrix.A returns matrix view
-#print(L.A)
-#eigW, eigV = np.linalg.eig(L.A)
+
+print(L.toarray())
+eigW, eigV = np.linalg.eig(L.A)
+
+#help.findBestEigen(eigV, Y, X)
 #print("Largest eigenvalue:", max(e))
 #print("Smallest eigenvalue:", min(e))
 
-#print(eigW)
-#print(eigV)
+print(eigW)
+#print(eigV[0])
 
 new_x = []
 new_y = []
@@ -45,10 +32,14 @@ for r in X:
     new_x.append(r[0])
     new_y.append(r[1])
 
-plt.scatter(new_x, new_y, c=Y)
+kmeans = KMeans(n_clusters=2)
+new_data = eigV[1].reshape(-1,1)
+
+kmeans.fit(new_data)
+plt.scatter(new_x, new_y, c=kmeans.labels_)
 plt.savefig("moons.png")
 
-pos=nx.get_node_attributes(G,'pos')
-ax = plt.subplot(121)
-nx.draw(G, pos)
-plt.savefig('graph.png')
+#pos=nx.get_node_attributes(moonsGraph,'pos')
+#ax = plt.subplot(121)
+#nx.draw(moonsGraph, pos)
+#plt.savefig('graph.png')
