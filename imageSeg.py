7eff97920c97eada55f6428f14f315ccee0fea99
Michael Wright <sc19mw@leeds.ac.uk>
2023-03-23 16:59:08 +0000

    some progress on the image segmentation

--- imageSeg.py
+++ imageSeg.py
@@ -6,28 +6,34 @@ import numpy as np
 
 def messing():
 
-    im = Image.open("./images/megaSmall.jpg")
+    im = Image.open("./images/banana.jpg")
     pixels = list(im.getdata())
     w, h = im.size
+    
     print(len(pixels))
-    G = create_graph(pixels, w, h)
+    #G = create_graph(pixels, w, h)
+    G = create_graph_fully_connected(pixels, w, h, 15)
+    
     print("graph made")
     Lnorm = nx.normalized_laplacian_matrix(G)
+    
+    
     print("normalized_laplacian_matrix made")
-    eigvalues, eigVectors = np.linalg.eig(Lnorm.A)
+    eigvalues, eigVectors = np.linalg.eigh(Lnorm.A)
+
     print("eigvalues, eigVectors made")
     eigens = util.sortEigens(eigvalues, eigVectors)
+    
     print("sortEigens done")
-    img = Image.new('RGB', (w, h))
     seg = util.second_eigenvector_segmentation(eigens["vectors"][1])
-    for x in range(w):
-        for y in range(h):
-            index = y * w + x
-            if seg[index] == 0:
-                img.putpixel((x, y), (255, 255, 255))
-            else:
-                img.putpixel((x, y), (0, 0, 0))
-    img.save('output.png')
+    total = 0
+    for i in seg:
+        if i == 1:
+            total += 1
+    print(total)
+    make_seg_img('output.png', w, h, seg)
+    
+
 
 def create_graph(pixels, w, h):
     G = nx.Graph()
@@ -58,6 +64,33 @@ def is_adjancent(pixel1, pixel2):
 def get_dist(a, b, c):
     return (a**2 + b**2 + c**2)**0.5
 
+def create_graph_fully_connected(pixels, w, h, r):
+    G = nx.Graph()
+    for i, val in enumerate(pixels):
+            G.add_node( i )
+
+    for i, RGB1 in enumerate(pixels):
+        for j, RGB2 in enumerate(pixels):
+            if j > i:
+                d = get_dist(RGB1[0]-RGB2[0], RGB1[1]-RGB2[1], RGB1[2]-RGB2[2])
+                x1 = i % w
+                y1 = i // w
+                x2 = j % w
+                y2= j // w
+
+                pixels_away = ( (x2-x1)**2 + (y2-y1)**2 )**0.5
+
+                colourVal = 2.781 ** ( -((d ** 2)) / 0.005 )
+
+                if pixels_away < r:
+                    distanceVal = 2.781 ** ( -((pixels_away ** 2)) / 15 )
+                else:
+                    distanceVal = 0
+
+                G.add_edge(i, j, weight = colourVal * distanceVal )
+        if i % 10 == 0:
+            print(i)
+    return G
 
 '''
 # Get the width and height of the image
@@ -69,3 +102,17 @@ y = 0
 r, g, b = pixels[y * width + x]
 
 '''
+
+def make_seg_img(path, w, h, segment):
+    img = Image.new('RGB', (w, h))
+
+    for x in range(w):
+        for y in range(h):
+            index = y * w + x
+            if segment[index] == 0:
+                img.putpixel((x, y), (255, 255, 255))
+            else:
+                img.putpixel((x, y), (0, 0, 0))
+
+
+    img.save(path)
--- main.py
+++ main.py
@@ -14,9 +14,9 @@ import imageSeg as img
 #twoMoon.run_test_with_plot(1000, 0.1, 0.2, 10, 500, "./plots/GL_Seg2.png")
 #twoMoon.run_test_with_plot(1000, 0.1, 1.33, 1.5, 500, "./plots/GL_Seg3.png")
 
-twoMoon.playground(1000, 50)
+#twoMoon.playground(1000, 50)
 
 #util.plot_two_moons(500, 0, "./plots/test1.png")
 #util.plot_two_moons(1000, 0.05, "./plots/test2.png")
 #util.plot_two_moons(1000, 0.15, "./plots/test3.png")
-#img.messing()
+img.messing()
